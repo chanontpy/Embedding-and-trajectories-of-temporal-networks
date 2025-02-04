73b605be29a4a6cd033c633f7ba74beaa4b8c9fc
chanontpy <100870030+chanontpy@users.noreply.github.com>
2025-02-04 18:57:18 +0700

    Update Tie-decay-network-and-distance-matrix.py

--- Tie-decay-network-and-distance-matrix.py
+++ Tie-decay-network-and-distance-matrix.py
@@ -135,3 +135,94 @@ def Squared_Laplacian_distance_matrix(B): # B = Tie_decay_matrices(alpha, C)
                 continue
             
     return LD
+
+def Centered_Distance_matrix(D): # D is a squared distance matrix
+    n = np.shape(D)[0]
+    m = np.shape(D)[1]
+    I = np.identity(n)
+    One_mat = np.ones((n,n))
+    H = I - (1/n * One_mat)
+    D_scaled = -0.5 * np.matmul(H,np.matmul(D,H))
+    return D_scaled
+
+def aux_delta(list_of_landmarks): # list_of_landmarks is a list containing landmarks
+    P = []
+    X = []
+    
+    for L in list_of_landmarks:
+        M = Find_eigenpair(L)
+        m = len(M)
+        Q = []
+        Y = []
+        for k in range(0,m):
+            Q.append(M[k][0])
+            Y.append(M[k][1])
+        P.append(Q)
+        X.append(Y)
+    
+    return P,X
+
+def Classical_MDS(D,k): # D is a squared distance matrix, k is the embedding dimension
+    L = []
+    #D = normalisation(D)
+    B = Centered_Distance_matrix(D)
+    #B = 1/np.std(B_ast) * B_ast
+    Lambda = Find_eigenpair(B) # Recall that the eigenvalues are already sorted.
+    total_sum = 0
+    neg_sum = 0
+    confidence = 0
+    sqrt_eigenvalue = []
+    for i in range(0,len(Lambda)):
+        if Lambda[i][0] > 0:
+            sqrt_eigenvalue.append(np.sqrt(Lambda[i][0]))
+
+    for i in range(0,k):
+        L.append(sqrt_eigenvalue[i] * Lambda[i][1])
+
+    return L, Lambda
+
+def Col_sum(D): # D is a squared distance matrix
+    N = np.shape(D)[1]
+    Col_D_sum = np.reshape(np.sum(D, axis = 1), (N,1))
+    Delta_mu = 1/N * Col_D_sum
+    
+    return Delta_mu
+
+#---LMDS starts here---#
+
+a_delta = aux_delta(list_of_landmarks)
+landmark_eigenvalues = a_delta[0]
+M = len(landmark_eigenvalues)
+Laplacian_dist_mat = np.zeros((M,M))
+Landmark_eigenpair = Classical_MDS(Laplacian_dist_mat,2)[1] #These eigenpairs already dependends on the choice of distance measure.
+delta_mu = Col_sum(Laplacian_dist_mat)
+
+N = np.shape(Laplacian_dist_mat)[1]
+inv_sqrt_eigenvalues = []
+L_prime_before = []
+k = 2
+for i in range(0,k):
+    if Landmark_eigenpair[i][0] > 0:
+        inv_sqrt_eigenvalues.append(1 / np.sqrt(Landmark_eigenpair[i][0]))
+    
+for i in range(0,k):
+    vi = inv_sqrt_eigenvalues[i] * Landmark_eigenpair[i][1]
+    L_prime_before = np.concatenate((L_prime_before,vi.tolist()), axis = 0)
+        
+L_prime = np.reshape(L_prime_before, (k,N)) # Pseudo inverse matrix of L
+
+def squared_dist_vector(t,landmark_eigenvalues,dictionary):
+    # "dictionary" is a python dictionary whose keys values are time steps 't' and tie-decay matrices at time 't'.
+    eigen_pair_arb = Find_eigenpair(Laplacian_matrix(dictionary[t]))
+    eigen_val_arb = np.array([i[0] for i in eigen_pair_arb])
+    sq_dist_vec = []
+    for i in landmark_eigenvalues:
+        sq_dist_vec.append(LA.norm(eigen_val_arb-np.array(i))**2)
+    return np.array(sq_dist_vec)
+
+def LMDS(t):
+    sq_vec = squared_dist_vector(t,landmark_eigenvalues,dictionary)
+    sq_vec = sq_vec.reshape(N,1)
+    vec = sq_vec - delta_mu
+    embed_coord =  -0.5*np.matmul(L_prime,vec)
+    return embed_coord
